e7a38342b5493b1f451db8828239aca30f619b1f
Alex Isaacs <alexisaacs18@gmail.com>
2023-12-01 17:01:04 -0500

    updated project

--- sales-forcasting/src/App.js
+++ sales-forcasting/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import Header from './Components/Header';
 import PipelineContainer from './Components/PipelineContainer';
 import Search from './Components/Search';
@@ -7,6 +8,20 @@ import './App.css';
 function App() {
 
   const url = "http://localhost:3000/pipeline"
+
+  const [deals, setDeals] = useState([])
+
+  useEffect(() => {
+    getDeals()
+  }, [])
+
+  function getDeals() {
+    fetch(url)
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }
+
+
   return (
     <div className="App">
       <Header />
--- sales-forcasting/src/Components/PipelineContainer.js
+++ sales-forcasting/src/Components/PipelineContainer.js
@@ -3,9 +3,33 @@ import Deals from './Deals';
 
 function PipelineContainer() {
     return (
-        <div className="PipelineContainer">
-            <Deals />
-        </div>
+        <table className="ui celled striped padded table">
+            <tbody>
+                <tr>
+                    <th>
+                        <h3 className="ui center aligned header">Date</h3>
+                    </th>
+                    <th>
+                        <h3 className="ui center aligned header">Name</h3>
+                    </th>
+                    <th>
+                        <h3 className="ui center aligned header">Rep</h3>
+                    </th>
+                    <th>
+                        <h3 className="ui center aligned header">Users</h3>
+                    </th>
+                    <th>
+                        <h3 className="ui center aligned header">MRR</h3>
+                    </th>
+                    <th>
+                        <h3 className="ui center aligned header">Stage</h3>
+                    </th>
+                    <th>
+                        <h3 className="ui center aligned header">Close Date</h3>
+                    </th>
+                </tr>
+            </tbody>
+        </table>
     );
 }
 
