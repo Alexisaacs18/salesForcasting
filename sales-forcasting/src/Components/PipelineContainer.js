983ad7bba44c2b946ccf6e802d998f89e4743b3b
Alex Isaacs <alexisaacs18@gmail.com>
2023-12-04 12:41:08 -0500

    updated search

--- sales-forcasting/src/Components/PipelineContainer.js
+++ sales-forcasting/src/Components/PipelineContainer.js
@@ -1,35 +1,39 @@
 import React from 'react';
 import Deals from './Deals';
+import Search from './Search';
 
 function PipelineContainer() {
     return (
-        <table className="ui celled striped padded table">
-            <tbody>
-                <tr>
-                    <th>
-                        <h3 className="ui center aligned header">Date</h3>
-                    </th>
-                    <th>
-                        <h3 className="ui center aligned header">Name</h3>
-                    </th>
-                    <th>
-                        <h3 className="ui center aligned header">Rep</h3>
-                    </th>
-                    <th>
-                        <h3 className="ui center aligned header">Users</h3>
-                    </th>
-                    <th>
-                        <h3 className="ui center aligned header">MRR</h3>
-                    </th>
-                    <th>
-                        <h3 className="ui center aligned header">Stage</h3>
-                    </th>
-                    <th>
-                        <h3 className="ui center aligned header">Close Date</h3>
-                    </th>
-                </tr>
-            </tbody>
-        </table>
+        <div>
+            <Search />
+            <table className="ui celled striped padded table">
+                <tbody>
+                    <tr>
+                        <th>
+                            <h3 className="ui center aligned header">Date</h3>
+                        </th>
+                        <th>
+                            <h3 className="ui center aligned header">Name</h3>
+                        </th>
+                        <th>
+                            <h3 className="ui center aligned header">Rep</h3>
+                        </th>
+                        <th>
+                            <h3 className="ui center aligned header">Users</h3>
+                        </th>
+                        <th>
+                            <h3 className="ui center aligned header">MRR</h3>
+                        </th>
+                        <th>
+                            <h3 className="ui center aligned header">Stage</h3>
+                        </th>
+                        <th>
+                            <h3 className="ui center aligned header">Close Date</h3>
+                        </th>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     );
 }
 
