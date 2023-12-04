ed6eef0f49a521f5910bb7c660df529ea474cef7
Alex Isaacs <alexisaacs18@gmail.com>
2023-12-04 12:00:19 -0500

    updates

--- sales-forcasting/src/App.js
+++ sales-forcasting/src/App.js
@@ -3,6 +3,9 @@ import { useState, useEffect } from 'react';
 import Header from './Components/Header';
 import PipelineContainer from './Components/PipelineContainer';
 import Search from './Components/Search';
+import RepPage from './Components/RepPage';
+import StagePage from './Components/StagePage';
+import ClosePage from './Components/ClosePage';
 import './App.css';
 
 function App() {
@@ -24,6 +27,10 @@ function App() {
 
   return (
     <div className="App">
+      <h1>Forecast.io</h1>
+      <RepPage />
+      <ClosePage />
+      <StagePage />
       <Header />
       <Search />
       <PipelineContainer />
