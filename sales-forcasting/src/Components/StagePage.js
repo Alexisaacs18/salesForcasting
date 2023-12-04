53c12b441b69f15822125a5228d73aaae7f0ccda
Alex Isaacs <alexisaacs18@gmail.com>
2023-12-04 17:25:16 -0500

    StagePage Change

--- sales-forcasting/src/Components/StagePage.js
+++ sales-forcasting/src/Components/StagePage.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 
 function StagePage() {
+
+    const stagesUrl = "http://localhost:3001/stages"
+
+    const [stage, setStage] = useState([])
+
+    useEffect(() => {
+        fetch(stagesUrl)
+            .then(res => res.json())
+            .then(data => setStage(data))
+    }, [])
+
+    console.log(stage)
+
     return (
         <div className="StagePage">
-
+            {stage.map((stage) => (
+                <h3 key={stage.id}>{`${stage.name}: Revenue`}</h3>
+            ))}
         </div>
     );
 }
