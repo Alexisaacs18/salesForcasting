926e2d4491cedaa8721d2fb9fe07f9248341adf5
Nick Sette <njs2394@gmail.com>
2023-12-05 15:07:50 -0500

    Sudo Code Update form

--- sales-forcasting/src/Components/Deal.js
+++ sales-forcasting/src/Components/Deal.js
@@ -5,6 +5,13 @@ function Deal() {
     const location = useLocation();
     const deal = location.state; // Access the state directly
 
+    // We have the current data through deal variable 
+
+    // 1) Creata a form in state 
+    // 2) setState to begin with the deal variable info
+    // 3) onSubmit, run PATCH through deal.id to update the existing data in the DB
+    // 4) Home will re-render every time we click on it so, the infomation here will update when come back to it. 
+
     return (
         <div>
             <h2>{deal.name}</h2>
