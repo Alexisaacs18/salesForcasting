42dfb87eac60bbdfc51ebaa65118700b574c3973
Alex Isaacs <alexisaacs18@gmail.com>
2023-12-06 21:37:04 -0500

    updated json

--- sales-forcasting/src/Components/RepPage.js
+++ sales-forcasting/src/Components/RepPage.js
@@ -6,10 +6,6 @@ function RepPage() {
     const dealsUrl = "http://localhost:3001/deals"
     const [dealReps, setDealReps] = useState([])
 
-    const nick = 'https://cdn.discordapp.com/attachments/1180185809832382465/1181678528073318541/Sette_Nicholas.jpg?ex=6581eee3&is=656f79e3&hm=1570e12b68abfa9ee2ba2ec3749c8005d3c56e2d71a3177f8db7f629f90a4b00&'
-    const sean = 'https://media.discordapp.net/attachments/1180185809832382465/1181679149740462080/pic_for_sales_forcasting.jpeg?ex=6581ef77&is=656f7a77&hm=d0c967dda48b6d43bac28e365572ba282535aa5a4b97ffa952b9447defff66c6&=&format=webp&width=1060&height=1060'
-    const alex = 'https://media.discordapp.net/attachments/1180185809832382465/1181680935616401549/IMG_1235.jpg?ex=6581f121&is=656f7c21&hm=a4ca45b4814c2722dabb22ede013097324b0aac01cc836859dbbe4e7c4e4a9d5&=&format=webp&width=934&height=1060'
-
     const repDataMap = {
         "Nick Sette": 'https://cdn.discordapp.com/attachments/1180185809832382465/1181678528073318541/Sette_Nicholas.jpg?ex=6581eee3&is=656f79e3&hm=1570e12b68abfa9ee2ba2ec3749c8005d3c56e2d71a3177f8db7f629f90a4b00&',
         "Sean Innella": 'https://media.discordapp.net/attachments/1180185809832382465/1181679149740462080/pic_for_sales_forcasting.jpeg?ex=6581ef77&is=656f7a77&hm=d0c967dda48b6d43bac28e365572ba282535aa5a4b97ffa952b9447defff66c6&=&format=webp&width=1060&height=1060',
